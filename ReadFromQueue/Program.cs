b4d5b23c41bcc9343666fba8756a03b6ef716ee1
Aditya Nag <adityanag@gmail.com>
2015-12-05 16:11:15 -0500

    Added a project that reads the queue

---
+++ ReadFromQueue/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Newtonsoft.Json;
+using System.Configuration;
+using InsertIntoQueue;
+
+namespace ReadFromQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageConnectionString"]);
+            var queueClient = storageAccount.CreateCloudQueueClient();
+            var queue = queueClient.GetQueueReference("tweetlyst");
+            var messages =  queue.GetMessages(32, TimeSpan.FromMinutes(2), null, null);
+            
+            foreach (var message in messages)
+            {
+                try
+                {
+                    // if processing was not possible, delete the messagecheck for unprocessable messages
+                    if (message.DequeueCount < 5)
+        {
+                        var ticketRequest = JsonConvert.DeserializeObject<TweetQueueSetter>(message.AsString);
+                        // process the ticket request (expensive operation)
+                        Console.WriteLine(ticketRequest);
+                    }
+        else
+        {
+                       Console.WriteLine("Processing failed.");
+                    }
+                    // delete message so that it becomes invisible for other workers
+                    queue.DeleteMessageAsync(message);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("message", e.Message);
+                }
+            }
+
+        }
+    }
+}
---
+++ ReadFromQueue/TweetQueueSetter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertIntoQueue
+{
+    class TweetQueueSetter
+    {
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public string UserId { get; set; }
+        public string AccessToken { get; set; }
+        public string AccessKey { get; set; }
+        public string SearchTerms { get; set; }
+        public string ControlAction { get; set; }
+        public DateTime Time { get; set; }
+
+    }
+}
